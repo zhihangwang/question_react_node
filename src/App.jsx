d3e1c5e3367112384617ef09308a31ed0d92878a
jason.wang <jason.wang@starlight-sms.com>
2019-03-05 13:49:23 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ const IssueRow = (props) => (
 
 function IssueTable(props) {
 	const issueRows = props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />);
+	console.log('2');
 	return (<table style={{ border: '1px' }}>
 		<thead>
 			<tr>
